950a28572076115f7ff55536222df2b808427521
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-23 15:16:41 +0500

    first calculator

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-function myplaceholder(){
-    var result = document.getElementById("inputField");
-    alert("hello world")
+function getNumber(num){
+    var myResult = document.getElementById("result");
+    myResult.value += num;
+}
+function clearResult(){
+    var myResult = document.getElementById("result");
+    myResult.value = ""
+}
+function getResult(){
+    var myResult = document.getElementById("result");
+    myResult.value = eval(myResult.value);
 }
